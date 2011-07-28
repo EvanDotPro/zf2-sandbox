ef9e0c45c2964b48b3283d0cc8914d9a605a4ecf
Evan Coury <me@evancoury.com>
2011-07-28 07:32:01 -0700

    initial commit; readme, index, and web init

---
+++ application/init-web.php
@@ -0,0 +1,45 @@
+<?php
+// Define path to application directory
+defined('APPLICATION_PATH')
+	|| define('APPLICATION_PATH',
+			realpath(dirname(__FILE__) . '/../application'));
+
+// Define path to library directory
+defined('LIBRARY_PATH')
+	|| define('LIBRARY_PATH',
+	        realpath(dirname(__FILE__) . '/../library'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+	|| define('APPLICATION_ENV',
+			(getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV')
+											: 'production'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(LIBRARY_PATH, get_include_path())));
+
+// Init autoloader
+require_once 'Zend/Loader/AutoloaderFactory.php';
+Zend\Loader\AutoloaderFactory::factory(array(
+    'Zend\Loader\ClassMapAutoloader' => array(
+        APPLICATION_PATH . '/.classmap.php',
+    ),
+    'Zend\Loader\StandardAutoloader' => array(
+        'namespaces' => array(
+            'Zf2'         => LIBRARY_PATH . '/Zf2',
+        ),
+    ),
+));
+
+// Configuration
+$config = new Zend\Config\Config(include APPLICATION . '/configs/config.php');
+foreach ($config->phpSettings as $key => $value) {
+    ini_set($key, $value);
+}
+
+// Create application, bootstrap, and run
+$bootstrap = new $config->bootstrap_class($config);
+$bootstrap->execute();
+
+$front   = new Zf2\Mvc\FrontController($bootstrap->getContainer());
+$front->dispatch(new Zf2\Http\Request())->send();
