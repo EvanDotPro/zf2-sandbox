3e1e7dc7b64a5510a0ba66da4753ca05695e640e
Evan Coury <me@evancoury.com>
2011-07-28 16:01:36 -0700

    removed db test sstuff from helloworld

--- modules/core.helloworld/application/Base/Controller/Index.php
+++ modules/core.helloworld/application/Base/Controller/Index.php
@@ -6,11 +6,5 @@ class Index extends ActionController
 {
     public function index()
     {
-        $db = self::$di->get('read-adapter');
-        $sql = $db->select()
-            ->from('multisite_site')
-            ->where('site_id = ?', 1);
-        $row = $db->fetchRow($sql);
-        return $row;
     }
 }
