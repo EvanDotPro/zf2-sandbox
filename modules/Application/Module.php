3ee7b64991e516a188e270ea4f67bb1befb8a28f
Evan Coury <me@evancoury.com>
2011-09-09 14:36:41 -0700

    updated zf2 submodule and added example for init.post event

--- modules/Application/Module.php
+++ modules/Application/Module.php
@@ -3,7 +3,8 @@
 namespace Application;
 
 use Zend\Config\Config,
-    Zend\Loader\AutoloaderFactory;
+    Zend\Loader\AutoloaderFactory,
+    Zend\EventManager\StaticEventManager;
 
 class Module
 {
@@ -15,6 +16,10 @@ class Module
     public function init()
     {
         $this->initAutoloader();
+        $events = StaticEventManager::getInstance();
+        $events->attach('Zf2Module\ModuleCollection', 'init.post', function($e) {
+            // This is ran after all modules' init() have ran
+        });
     }
 
     protected function initAutoloader()
