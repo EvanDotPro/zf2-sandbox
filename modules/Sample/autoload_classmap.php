e5e144a66ebaf139990676ebb507ed4d0879f89d
Evan Coury <me@evancoury.com>
2011-09-23 12:38:21 -0700

    Added previously ignored files due to bad gitignore

---
+++ modules/Sample/autoload_classmap.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+  'Sample\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
+  'Sample\\Controller\\SampleController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Sample' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'SampleController.php',
+);
