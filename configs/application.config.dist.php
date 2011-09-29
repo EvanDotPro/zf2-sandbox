d4e925f8754e487bab32059d32a1ee538f899fad
Evan Coury <me@evancoury.com>
2011-09-29 14:34:41 -0700

    Update dist config file for latest zf2 master changes

--- configs/application.config.dist.php
+++ configs/application.config.dist.php
@@ -2,11 +2,8 @@
 return new Zend\Config\Config(array(
     'module_paths' => array(
         realpath(__DIR__ . '/../modules'),
-        realpath(__DIR__ . '/../library/ZendFramework2/modules'),
     ),
     'modules' => array(
-        'ZendModule', // paradox?!
-        'ZendMvc',
         'Application',
         'Sample',
     ),
