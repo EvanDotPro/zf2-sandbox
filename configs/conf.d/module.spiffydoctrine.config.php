e03b3652b401e8bc1ee27397cab665916b208de2
Evan Coury <me@evancoury.com>
2011-11-19 16:31:36 -0700

    Using module-supplied config files now

---
+++ configs/conf.d/module.spiffydoctrine.config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * SpiffyDoctrine Configuration
+ */
+$spiffyDoctrineConfiguration = array(
+    /**
+     * PDO Connection DI alias
+     *
+     * Specify the DI alias for the configured PDO instance that SpiffyDoctrine 
+     * should use.
+     */
+    'pdo' => 'masterdb',
+
+    /**
+     * End of SpiffyDoctrine configuration
+     */
+);
+
+/**
+ * You do not need to edit below this line
+ */
+return array(
+    'di' => array(
+        'instance' => array(
+            'doctrine_connection' => array(
+                'parameters' => array(
+                    'pdo' => $spiffyDoctrineConfiguration['pdo'],
+                ),
+            ),
+        ),
+    ),
+);
